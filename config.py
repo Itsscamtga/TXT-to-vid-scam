50733c7f74368d42234318f6bbdb5bcf8232f411
Itsscamtga <mujeem008@gmail.com>
2024-09-03 08:51:51 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7363610742:AAHRdYOwGwg63pc3xnnFh1ZsEDDsOdhTpOM")
+    API_ID = int(os.environ["API_ID", 21179966]
+    API_HASH = os.environ["API_HASH", ""]
+    AUTH_USERS = "7326397503"""
+ 
